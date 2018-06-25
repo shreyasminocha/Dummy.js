a2ddcffed7947742565e7da48a0af9d2f097a3e9
Shreyas Minocha <shreyasminocha7@gmail.com>
2018-06-25 16:54:24 +0530

    Add tests
    Resolves #6

--- src/dummy.js
+++ src/dummy.js
@@ -1,40 +1,30 @@
 'use strict';
 
 (() => {
-    const wordBank = 'a,ac,accumsan,ad,adipiscing,aenean,aliquam,aliquet,amet,ante,aptent,arcu,at,auctor,augue,bibendum,blandit,commodo,condimentum,congue,consectetur,consequat,conubia,convallis,cras,cubilia,curabitur,curae,cursus,dapibus,diam,dictum,dignissim,dolor,donec,dui,duis,efficitur,egestas,eget,eleifend,elementum,elit,enim,erat,eros,est,et,etiam,eu,euismod,ex,facilisis,fames,faucibus,felis,fermentum,feugiat,finibus,fringilla,fusce,gravida,hendrerit,himenaeos,iaculis,id,imperdiet,in,inceptos,integer,interdum,ipsum,justo,lacinia,lacus,laoreet,lectus,leo,libero,ligula,litora,lobortis,lorem,luctus,maecenas,magna,malesuada,massa,mattis,mauris,maximus,metus,mi,molestie,mollis,morbi,nam,nec,neque,nibh,nisi,nisl,non,nostra,nulla,nullam,nunc,odio,orci,ornare,pellentesque,per,pharetra,phasellus,placerat,porta,porttitor,posuere,praesent,pretium,primis,proin,pulvinar,purus,quam,quis,quisque,rhoncus,risus,rutrum,sagittis,sapien,scelerisque,sed,sem,semper,sit,sociosqu,sodales,sollicitudin,suscipit,suspendisse,taciti,tellus,tempus,tincidunt,torquent,tortor,tristique,turpis,ullamcorper,ultrices,ultricies,urna,ut,varius,vehicula,vel,velit,venenatis,vestibulum,vitae,vivamus,viverra,volutpat,vulputate'.split(',');
-
     function initialize() {
         let elements = document.querySelectorAll('[data-dummy]');
 
         for (let element of elements) {
-            const config = parseConfig(element.getAttribute('data-dummy'));
-            element.innerText = generate(config.type, config.count);
+            const config = element.getAttribute('data-dummy');
+            config = config.split(' ');
+            element.innerText = generate(config[1], config[0]);
         }
     }
 
-    function parseConfig(config) {
-        const segments = config.split(' ');
-
-        return {
-            count: segments[0],
-            type: segments[1]
-        };
-    }
+    window.onload = initialize;
 
     function generate(type, count) {
         let fill = '';
 
         switch (type) {
-            case 'word':
-            case 'words':
+            case 'word': case 'words':
                 for (let i = 0; i < count; i++) {
                     fill += randomElement(wordBank) + ' ';
                 }
 
                 fill = sentenceCase(fill.trim());
                 break;
-            case 'sentence':
-            case 'sentences':
+            case 'sentence': case 'sentences':
                 for (let i = 0; i < count; i++) {
                     fill += sentenceCase(
                         generate('words', randomInteger(5, 12)),
@@ -44,8 +34,7 @@
                 }
 
                 break;
-            case 'paragraph':
-            case 'paragraphs':
+            case 'paragraph': case 'paragraphs':
                 for (let i = 0; i < count; i++) {
                     fill += generate('sentences', randomInteger(8, 14));
                     fill += '\n';
@@ -56,15 +45,12 @@
                 console.error(`Error: Invalid dummy fill type '${type}'`);
         }
 
-        return fill;
+        return fill.trim();
     }
 
-    window.onload = initialize;
+    const wordBank = 'a,ac,accumsan,ad,adipiscing,aenean,aliquam,aliquet,amet,ante,aptent,arcu,at,auctor,augue,bibendum,blandit,commodo,condimentum,congue,consectetur,consequat,conubia,convallis,cras,cubilia,curabitur,curae,cursus,dapibus,diam,dictum,dignissim,dolor,donec,dui,duis,efficitur,egestas,eget,eleifend,elementum,elit,enim,erat,eros,est,et,etiam,eu,euismod,ex,facilisis,fames,faucibus,felis,fermentum,feugiat,finibus,fringilla,fusce,gravida,hendrerit,himenaeos,iaculis,id,imperdiet,in,inceptos,integer,interdum,ipsum,justo,lacinia,lacus,laoreet,lectus,leo,libero,ligula,litora,lobortis,lorem,luctus,maecenas,magna,malesuada,massa,mattis,mauris,maximus,metus,mi,molestie,mollis,morbi,nam,nec,neque,nibh,nisi,nisl,non,nostra,nulla,nullam,nunc,odio,orci,ornare,pellentesque,per,pharetra,phasellus,placerat,porta,porttitor,posuere,praesent,pretium,primis,proin,pulvinar,purus,quam,quis,quisque,rhoncus,risus,rutrum,sagittis,sapien,scelerisque,sed,sem,semper,sit,sociosqu,sodales,sollicitudin,suscipit,suspendisse,taciti,tellus,tempus,tincidunt,torquent,tortor,tristique,turpis,ullamcorper,ultrices,ultricies,urna,ut,varius,vehicula,vel,velit,venenatis,vestibulum,vitae,vivamus,viverra,volutpat,vulputate'.split(',');
 
-    //////////////////////
-    // Helper Functions //
-    //////////////////////
-
+    // inclusive
     function randomInteger(min = 0, max = 1) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
---
+++ test/tests.js
@@ -0,0 +1,55 @@
+import test from 'ava';
+import puppeteer from 'puppeteer';
+
+let browser, page;
+
+test.before('Open the test page in browser', async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+
+    await page.goto(`file:///${__dirname}/test.html`);
+});
+
+test('15 words', async t => {
+    let words = await page.$eval('[data-dummy="15 words"]', element => {
+        return element.innerText;
+    });
+
+    words = words.split(' ');
+
+    t.is(words.length, 15);
+});
+
+test('10 sentences', async t => {
+    let sentences = await page.$eval('[data-dummy="10 sentences"]', element => {
+        return element.innerText;
+    });
+
+    sentences = sentences.split('. ');
+
+    t.is(sentences.length, 10);
+
+    for (const sentence of sentences) {
+        const words = sentence.split(' ');
+        t.true(words.length >= 5 && words.length <= 12);
+    }
+});
+
+test('3 paragraphs', async t => {
+    let paras = await page.$eval('[data-dummy="3 paragraphs"]', element => {
+        return element.innerText;
+    });
+
+    paras = paras.split('\n');
+
+    t.is(paras.length, 3);
+
+    for (const para of paras) {
+        const sentences = para.split('. ');
+        t.true(sentences.length >= 8 && sentences.length <= 14);
+    }
+});
+
+test.after.always('Close browser', () => {
+    browser.close()
+});
